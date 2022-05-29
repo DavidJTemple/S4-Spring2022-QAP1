7763b3fdec7f1df3b67b6fd8062c941e8a679238
DavidJTemple <96357555+DavidJTemple@users.noreply.github.com>
2022-05-29 16:12:55 -0230

    Add files via upload

---
+++ TestDrivenDev/src/test/java/com/keyin/testdrivendevelopment/EmployeeTest.java
@@ -0,0 +1,24 @@
+package com.keyin.testdrivendevelopment;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class EmployeeTest {
+
+    @Test
+    public void testNoEmployeeName(){
+        Employee employee = new Employee();
+        Assertions.assertEquals("", "");
+
+    }
+    @Test
+    public void testEmployeeFirstName(){
+        Employee employee = new Employee("David", "Temple");
+        Assertions.assertEquals("David", employee.getFirstName());
+    }
+    @Test
+    public void testEmployeeLastName(){
+        Employee employee = new Employee("David", "Temple");
+        Assertions.assertNotNull("Temple", employee.getLastName());
+    }
+}
